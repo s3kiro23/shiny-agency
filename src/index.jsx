a7b2261ca069c6af785550c30db17a519771945b
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-01-04 18:00:51 +0100

    add React Router

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
+import ClientForm from './components/ClientForm'
+import FreelanceForm from './components/FreelanceForm'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />}>
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
