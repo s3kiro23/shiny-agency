a0556b612971c14714e088338b7fc498f80a438a
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-01-04 18:16:21 +0100

    handling page 404

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
-import ClientForm from './components/ClientForm'
-import FreelanceForm from './components/FreelanceForm'
+import Error from './components/Error'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,10 +12,8 @@ ReactDOM.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />}>
-          <Route path="client" element={<ClientForm />} />
-          <Route path="freelance" element={<FreelanceForm />} />
-        </Route>
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>,
--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,14 +1,13 @@
-import {Outlet, Link} from "react-router-dom";
+import { useParams } from 'react-router-dom'
 
-function Survey(){
+function Survey() {
+    const { questionNumber } = useParams()
     return (
         <div>
             <h1>Questionnaire 🧮</h1>
-            <Link to="/client">Questionnaire Client</Link>
-            <Link to="/freelance">Questionnaire Freelance</Link>
-            <Outlet/>
+            <h2>Question {questionNumber}</h2>
         </div>
     )
 }
 
-export default Survey;
+export default Survey
