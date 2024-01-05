b5aa73e256c6e63bdd7a9124de27b7a045960511
Jm GERONIMI <spawn2x@protonmail.com>
2024-01-05 20:15:32 +0100

    update render version

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
@@ -15,7 +15,10 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Router>
       <GlobalStyle/>
@@ -29,5 +32,4 @@ ReactDOM.render(
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
 )
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atom'
+
+const SurveyContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+    text-decoration: underline;
+    text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+    margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+    padding-top: 30px;
+    & a {
+        color: black;
+    }
+    & a:first-of-type {
+        margin-right: 20px;
+    }
+`
 
 function Survey() {
     const { questionNumber } = useParams()
@@ -7,18 +37,56 @@ function Survey() {
     const prevQuestionNumber =
         questionNumberInt === 1 ? 1 : questionNumberInt - 1
     const nextQuestionNumber = questionNumberInt + 1
+    const [surveyData, setSurveyData] = useState({})
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    // useEffect(() => {
+    //     setDataLoading(true)
+    //     fetch('http://localhost:8000/survey')
+    //         .then((response) => response.json())
+    //         .then(({ surveyData }) => {
+    //             setSurveyData(surveyData)
+    //             setDataLoading(false)
+    //         })
+    //         .catch((error) => console.log(error))
+    // }, [])
+
+    // methode moderne avec asyn/await
+    useEffect(() => {
+        async function fetchSurvey() {
+            setDataLoading(true)
+            try {
+                const response = await fetch('http://localhost:8000/survey')
+                const { surveyData } = await response.json()
+                setSurveyData(surveyData)
+            } catch (error) {
+                console.log(error)
+                setError(true)
+            } finally {
+                setDataLoading(false)
+            }
+        }
+        fetchSurvey()
+    }, [])
 
     return (
-        <div>
-            <h1>Questionnaire 🧮</h1>
-            <h2>Question {questionNumber}</h2>
-            <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-            {questionNumberInt === 10 ? (
-                <Link to="/results">Résultats</Link>
+        <SurveyContainer>
+            <QuestionTitle>Question {questionNumber}</QuestionTitle>
+            {isDataLoading ? (
+                <Loader />
             ) : (
-                <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+                <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
             )}
-        </div>
+            <LinkWrapper>
+                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+                {surveyData[questionNumberInt + 1] ? (
+                    <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+                ) : (
+                    <Link to="/results">Résultats</Link>
+                )}
+            </LinkWrapper>
+        </SurveyContainer>
     )
 }
 
